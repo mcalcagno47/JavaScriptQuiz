0f4502b539038036dc5199ed14d0aa2c72f5db17
mcalcagno47 <110995252+mcalcagno47@users.noreply.github.com>
2022-10-21 16:41:21 -0500

    adjusted css and coded timer as best as possible for now

--- script.js
+++ script.js
@@ -3,28 +3,26 @@ var beginButton = document.getElementById('.begin-button');
 var mainEl = document.getElementById('.main');
 var questionEl = document.getElementById('.question');
 
-function countdown() {
-    var timeLeft = 75;
-    var timeInterval = setInterval(function () {
-        // As long as the `timeLeft` is greater than 1
-        if (timeLeft > 1) {
-            // Set the `textContent` of `timerEl` to show the remaining seconds
-            timerEl.textContent = timeLeft + ' seconds';
-            // Decrement `timeLeft` by 1
-            timeLeft--;
-        } else if (timeLeft === 1) {
-            // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-            timerEl.textContent = timeLeft + ' second';
-            timeLeft--;
-        } else {
-            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-            timerEl.textContent = '';
-            // Use `clearInterval()` to stop the timer
-            clearInterval(timeInterval);
-            // Call the `displayMessage()` function
-            displayMessage();
-        }
-    }, 1000);
-}
-console.log(countdown);
-countdown();
+var timer;
+var timerCount;
+
+function startTimer() {
+    timer= setInterval(function () {
+        timerCount--;
+        timerEl.textContent = timerCount;
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+              // Clears interval and stops timer
+              clearInterval(timer);
+              winGame();
+            }
+          }
+          // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            loseGame();
+          }
+        }, 1000);
+      }
